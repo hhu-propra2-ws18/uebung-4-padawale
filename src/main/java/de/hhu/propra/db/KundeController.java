c1ee73dc633fcbda5a9b14cc3ca7ca91e9311ccd
tiric103 <tim.richter99@web.de>
2018-11-15 15:23:02 +0100

    irgendwas

--- src/main/java/de/hhu/propra/db/KundeController.java
+++ src/main/java/de/hhu/propra/db/KundeController.java
@@ -1,10 +1,13 @@
 package de.hhu.propra.db;
 
 import de.hhu.propra.db.data.KundeRepository;
+import de.hhu.propra.db.entities.Kunde;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class KundeController{
@@ -14,6 +17,8 @@ public class KundeController{
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("kunden" , kunden);
+        List<Kunde> customers = (List<Kunde>) kunden.findAll();
+        model.addAttribute("kunden", customers);
         return "index";
     }
 }
