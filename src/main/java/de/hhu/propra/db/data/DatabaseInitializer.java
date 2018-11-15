cc95ccf95738898cc3075efdc73fd4157b522f88
tiric103 <tim.richter99@web.de>
2018-11-15 14:46:52 +0100

    Update DatabaseInitializer.java

--- src/main/java/de/hhu/propra/db/data/DatabaseInitializer.java
+++ src/main/java/de/hhu/propra/db/data/DatabaseInitializer.java
@@ -1,11 +1,18 @@
 package de.hhu.propra.db.data;
 
+import com.github.javafaker.Faker;
+import de.hhu.propra.db.entities.Auto;
+import de.hhu.propra.db.entities.Kunde;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseInitializer implements ServletContextInitializer{
@@ -15,22 +22,22 @@ public class DatabaseInitializer implements ServletContextInitializer{
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-//        System.out.println("Populating the database");
-//        final Faker faker = new Faker(Locale.GERMAN);
-//        IntStream.range(0, 30).forEach(value -> {
-//            final Kunde kunde = new Kunde();
-//            kunde.setName(faker.name().fullName());
-//            kunde.setStadt(faker.address().cityName());
-//            kunde.setPlz(Integer.parseInt(faker.address().zipCode()));
-//            final List<Auto> autos = IntStream.range(0, faker.number().numberBetween(0, 3)).mapToObj(j -> {
-//                Auto a = new Auto();
-//                a.setFarbe(faker.color().name());
-//                a.setMarke(faker.company().name());
-//                a.setHubraum(faker.number().numberBetween(1000, 2000));
-//                return a;
-//            }).collect(Collectors.toList());
-//            kunde.setAutos(autos);
-//            this.kundeRepository.save(kunde);
-//        });
+        System.out.println("Populating the database");
+        final Faker faker = new Faker(Locale.GERMAN);
+        IntStream.range(0, 30).forEach(value -> {
+            final Kunde kunde = new Kunde();
+            kunde.setName(faker.name().fullName());
+            kunde.setStadt(faker.address().cityName());
+            kunde.setPlz(Integer.parseInt(faker.address().zipCode()));
+            final List<Auto> autos = IntStream.range(0, faker.number().numberBetween(0, 3)).mapToObj(j -> {
+                Auto a = new Auto();
+                a.setFarbe(faker.color().name());
+                a.setMarke(faker.company().name());
+                a.setHubraum(faker.number().numberBetween(1000, 2000));
+             return a;
+          }).collect(Collectors.toList());
+           kunde.setAutos(autos);
+           this.kundeRepository.save(kunde);
+        });
     }
 }
