74c17ea5a814e97299fac8f85ab7b56e8955871a
tiric103 <tim.richter99@web.de>
2018-11-15 14:40:43 +0100

    Auto hinzugefügt

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,23 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Auto{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String marke;
+
+    private String farbe;
+
+    private int hubraum;
 }
